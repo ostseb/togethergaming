f292bbb228fab745434c76d5ebaaa05240a89932
Sebastian Österberg <sebastian.osterberg@gmail.com>
2020-09-13 19:06:06 +0200

    New token

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   target: 'server',
   env: {
     "CONTENTFUL_SPACE_ID": "qeqapgmln7mo",
-    "CONTENTFUL_ACCESS_TOKEN": "WzT-dWu7w1UrxFskmAzZcdmnuyubtfO15WqO9W7xPCE"
+    "CONTENTFUL_ACCESS_TOKEN": "gU0mqo9cQIB5furjRxJuN2CN7ExHdoDuFkICnVAgSL4"
   },
 }
